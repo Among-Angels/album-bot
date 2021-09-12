8fb7e28867cee38be020e953f2b69207a6f9ce44
Takahiro Hatakeyama <thatakemt@gmail.com>
2021-09-12 15:15:19 +0900

    既存アルバムに新規URLを追加する関数作成

--- album.go
+++ album.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 // Albumはタイトルとそれに紐付けられた画像URLの集合です。
@@ -33,4 +34,33 @@ func GetAlbumUrls(title string) (urls []string, e error) {
 	}
 	e = fmt.Errorf("アルバム%sが見つかりませんでした。", title)
 	return nil, e
+}
+
+// PostAlbumUrlは与えられたアルバム名のUrl配列に与えられたUrlを追加します
+// ファイル全部読んで全部上書きする脳筋処理なので改良の余地ありです。。
+func PostAlbumUrl(albumTitle, url string) (e error) {
+	raw, err := ioutil.ReadFile("./dataSet.json")
+	if err != nil {
+		return err
+	}
+
+	var albums Albums
+	json.Unmarshal(raw, &albums)
+	for index, album := range albums.Albums {
+		if album.Title != albumTitle {
+			continue
+		}
+		albums.Albums[index].Urls = append(albums.Albums[index].Urls, url)
+		marshaled, err := json.Marshal(albums)
+		if err != nil {
+			return err
+		}
+		writeError := ioutil.WriteFile("dataSet.json", marshaled, os.ModePerm)
+		if writeError != nil {
+			return writeError
+		}
+		return
+	}
+	e = fmt.Errorf("アルバム%sが見つかりませんでした。", albumTitle)
+	return e
 }
