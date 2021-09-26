cc3e2a2715fddbf64166b1428f5638ddf1c72cdd
Takahiro Hatakeyama <thatakemt@gmail.com>
2021-09-26 18:06:54 +0900

    指定アルバムの指定indexから指定数のURLを取得する関数作成

--- album.go
+++ album.go
@@ -36,6 +36,37 @@ func GetAlbumUrls(title string) (urls []string, e error) {
 	return nil, e
 }
 
+// GetAlbumPageは与えられたアルバム名と開始index,数量からURLのリストを返します
+func GetAlbumPage(title string, start, count int) (urls []string, e error) {
+
+	if start < 0 {
+		e = fmt.Errorf("startは0以上の数値を指定してください")
+		return nil, e
+	}
+
+	if count < 1 {
+		e = fmt.Errorf("countは1以上の数値を指定してください")
+		return nil, e
+	}
+
+	allUrls, err := GetAlbumUrls(title)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(allUrls)-1 < start {
+		e = fmt.Errorf("startはアルバム内のURL数より小さい値を指定してください")
+		return nil, e
+	}
+
+	// count込で溢れた場合末尾まで返す(この仕様でok...?)
+	if len(allUrls) < start+count {
+		return allUrls[start:], nil
+	}
+
+	return allUrls[start : start+count], nil
+}
+
 // PostAlbumUrlは与えられたアルバム名のUrl配列に与えられたUrlを追加します
 // ファイル全部読んで全部上書きする脳筋処理なので改良の余地ありです。。
 func PostAlbumUrl(albumTitle, url string) (e error) {
