61845da5fac705c638d8c3b3636e6cdb82f617e8
marukaz <kopamaru@gmail.com>
2021-10-03 23:40:33 +0900

    Change dataset path

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # albumbot
 
-### botの起動方法（ローカル版）
-・事前準備
+## botの起動方法（ローカル版）
+
+### 事前準備
+
 .envをmain.goと同じ階層に配置する。.envの書きぶりは以下参考リンク
 https://discordapp.com/channels/252122237761486849/788388972825018368/891657884570619934
 
-・起動コマンド
-$ go run main.go  
--booted!が出たら成功
+### 起動コマンド
+
+```sh
+go run ./cmd/albumbot/main.go  
+```
+
+booted!が出たら成功
--- album_test.go
+++ album_test.go
@@ -7,11 +7,8 @@ import (
 
 func TestGetAlbumUrls(t *testing.T) {
 	wants := []string{
-		"https://cdn.discordapp.com/attachments/723903982745157723/747799894001189024/00100lPORTRAIT_00100_BURST20190525180748800_COVER.jpg",
-		"https://cdn.discordapp.com/attachments/723903982745157723/782582360688033842/image0.jpg",
-		"https://cdn.discordapp.com/attachments/723903982745157723/784664833860960266/PXL_20201128_013520140.jpg",
-		"https://cdn.discordapp.com/attachments/723903982745157723/788395055843901450/image0.jpg",
-		"https://cdn.discordapp.com/attachments/723903982745157723/853546288527441940/unknown.png",
+		"https://test1.png",
+		"https://test2.png",
 	}
 	type args struct {
 		title string
@@ -23,13 +20,17 @@ func TestGetAlbumUrls(t *testing.T) {
 	}{{
 		name: "test",
 		args: args{
-			title: "taisho",
+			title: "test",
 		},
 		want: wants,
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, _ := GetAlbumUrls(tt.args.title); !reflect.DeepEqual(got, tt.want) {
+			got, err := GetAlbumUrls(tt.args.title)
+			if err != nil {
+				panic(err)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetAlbumUrls() = %v, want %v", got, tt.want)
 			}
 		})
